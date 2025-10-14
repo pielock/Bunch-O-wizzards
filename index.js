ded4fd26ed9d9d208ec0196829166bd9a29f4500
Jamal Raja <Jamalraja083@gmail.com>
2025-10-14 20:10:32 +0100

    Co-authored-by: Steven Kane <pielock@users.noreply.github.com> Co-authored-by: ZG1203 <ZG1203@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Bunch O'Wizards!");
+async function fetchData() {
+  try {
+    const pokemonName = document
+      .getElementById("pokemonName")
+      .value.toLowerCase();
+
+    const response = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+    );
+
+    if (!response.ok) {
+      throw new Error("Could not fetch resource");
+    }
+
+    const data = await response.json();
+    const pokemonSprite = data.sprites.front_default;
+    const imgEl = document.getElementById("pokemonSprite");
+
+    imgEl.src = pokemonSprite;
+    imgEl.style.display = "block";
+  } catch (err) {
+    console.error(err);
+  }
+}
